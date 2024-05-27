c8f49dfbda3c580a8d5d7db7a4a8f9a35f18a131
adambarta <adam.barta404@gmail.com>
2024-05-27 19:54:42 +0200

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { TooltipProvider } from "@/components/plate-ui/tooltip";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <TooltipProvider disableHoverableContent delayDuration={500} skipDelayDuration={0}>
+          <div className="relative flex min-h-screen flex-col">
+            <div className="flex-1">{children}</div>
+          </div>
+        </TooltipProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,100 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React, { useMemo, useCallback, useState, useEffect } from "react";
+import { BaseRange, createEditor, Node } from "slate";
+import { Editable, Slate, withReact } from "slate-react";
+import { withHistory } from "slate-history";
+import { createPlugins, ELEMENT_PARAGRAPH, createPluginFactory } from "@udecode/plate";
+import Typo from "typo-js";
+import { DecorateEntry, ENodeEntry, Plate, PlateEditor, Value } from "@udecode/plate-common";
+import { Editor } from "@/components/plate-ui/editor";
+
+type TypoDictionary = Typo | null;
+
+// Load Typo.js dictionary
+const loadDictionary = async () => {
+  const affResponse = await fetch("/dictionaries/cs_CZ.aff");
+  const affData = await affResponse.text();
+
+  const dicResponse = await fetch("/dictionaries/cs_CZ.dic");
+  const dicData = await dicResponse.text();
+
+  return new Typo("cs_CZ", affData, dicData);
+};
+
+const checkSpelling = (entry: ENodeEntry<Value>, dictionary: TypoDictionary): BaseRange[] => {
+  const [node, path] = entry;
+
+  if (!Node.string(node)) {
+    return [];
+  }
+
+  const ranges: BaseRange[] = [];
+  const text = Node.string(node);
+  // Regular expression to match words and exclude punctuation
+  // Regular expression to match words including diacritics
+  const wordRegex = /\b[\p{L}]+\b/gu;
+  const words = text.match(wordRegex);
+
+  if (!words) {
+    return ranges;
+  }
+
+  let start = 0;
+  words.forEach((word) => {
+    const wordStart = text.indexOf(word, start);
+    const wordEnd = wordStart + word.length;
+
+    // If word is spelled incorrectly, highlight it
+    if (dictionary && !dictionary.check(word)) {
+      console.log("Checking word against dictionary", word, dictionary.check(word));
+      ranges.push({
+        anchor: { path, offset: wordStart },
+        focus: { path, offset: wordEnd },
+        spellError: true, // Custom property to highlight errors
+      });
+    }
+
+    // Update start to the end of the current word plus one to account for spaces/punctuation
+    start = wordEnd + 1;
+  });
+
+  console.log("returning ranges", ranges);
+
+  return ranges;
+};
+
+const useSpellCheckPlugin = (dictionary: TypoDictionary) => {
+  return createPluginFactory({
+    key: "spellCheck",
+    decorate: (editor) => (value) => checkSpelling(value, dictionary),
+  })();
+};
+
+const TextEditor = () => {
+  const [dictionary, setDictionary] = useState<TypoDictionary>(null);
+
+  useEffect(() => {
+    loadDictionary().then((value) => {
+      setDictionary(value);
+      console.log("Loaded dictionary", value);
+    });
+  }, []);
+
+  if (!dictionary) {
+    return <div>Loading dictionary...</div>;
+  }
+
+  const spellCheckPlugin = useSpellCheckPlugin(dictionary);
+  const plugins = createPlugins([spellCheckPlugin]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="container mt-2">
+      <Plate plugins={plugins}>
+        <Editor placeholder="Enter some text..." />
+      </Plate>
     </main>
   );
-}
+};
+
+export default TextEditor;
