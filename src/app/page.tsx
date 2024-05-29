bb66c1ff6ffc12bafa5d69a58bc23090999bc741
adambarta <adam.barta404@gmail.com>
2024-05-29 19:01:57 +0200

    fix: lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,35 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TextEditor from "@/components/text-editor/text-editor";
+import Typo from "typo-js";
+
+export type TypoDictionary = Typo | null;
+
+// Load Typo.js dictionary
+const loadDictionary = async () => {
+  const affResponse = await fetch("/dictionaries/cs_CZ.aff");
+  const affData = await affResponse.text();
+
+  const dicResponse = await fetch("/dictionaries/cs_CZ.dic");
+  const dicData = await dicResponse.text();
+
+  return new Typo("cs_CZ", affData, dicData);
+};
 
 const HomePage = () => {
+  const [dictionary, setDictionary] = useState<TypoDictionary>(null);
+
+  useEffect(() => {
+    loadDictionary().then((value) => {
+      setDictionary(value);
+    });
+  }, []);
+
   return (
     <main className="container mt-20 mx-auto gap-4 flex flex-col">
       <h1 className="text-center text-4xl font-bold">Korektorr</h1>
-      <TextEditor />
+      {dictionary ? <TextEditor dictionary={dictionary} /> : <p>Loading dictionary...</p>}
     </main>
   );
 };
